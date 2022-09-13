a83b4c769adcfe071ff8bc64b11923458eae8ece
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2022-09-12 20:20:24 -0400

    jQuery 105 - Assignment 1
    Assignment 1 submission

--- scripts/register.js
+++ scripts/register.js
@@ -1,16 +1,17 @@
 // add payment method using a select element on the html
 
 //object constructor
-function User(email,password,name,age,gender,address,phone,color,payment) {
+function User(email,password,firstName,lastName,age,gender,address,phone,payment,color) {
     this.email=email;
     this.password=password;
-    this.name=name;
+    this.firstName=firstName;
+    this.lastName=lastName;
     this.age=age;
     this.gender=gender;
     this.address=address;
     this.phone=phone;
-    this.color=color;
     this.payment=payment;
+    this.color=color;
 }
 
 //getting the values from the inputs
@@ -19,24 +20,23 @@ function User(email,password,name,age,gender,address,phone,color,payment) {
 function register() {
     let txtEmail = $("userEmail").val();
     let txtPassword = $("userPassword").val();
-    let txtName = $("#userName").val();
+    let txtFirstName = $("#userFirstName").val();
+    let txtLastName = $("#userLastName").val();
     let txtAge = $("#userAge").val();
     let txtGender = $("#userGender").val();
     let txtAddress = $("#userAddress").val();
     let txtPhone = $("#userPhone").val();
-    let txtPayment = $("#userPayment").val();
+    let selPayment = $("#userPayment").val();
     let selColor = $("#userColor").val();
     
     //create the user obj
-    let aUser = new User(txtEmail, txtPassword, txtName, txtAge, txtGender, txtAddress, txtPhone, selColor);
+    let aUser = new User(txtEmail,txtPassword,txtFirstName,txtLastName,txtAge,txtGender,txtAddress,txtPhone,selPayment,selColor);
     //display the object in the console
 
     console.log(aUser);
     //clear the inputs
     $("input").val("");
 }
-
-
 
 function init() {
     //hook event
